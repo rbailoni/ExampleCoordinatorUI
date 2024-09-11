3b030db87349cb19e209e23070f88405a18b6b5b
Ricardo Bailoni <ricardo.bailoni@bancodaycoval.com.br>
2024-09-11 15:00:14 -0300

    Initial Commit

---
+++ ExampleCoordinatorUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExampleCoordinatorUI
+//
+//  Created by Ricardo Bailoni on 11/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExampleCoordinatorUI/ExampleCoordinatorUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleCoordinatorUIApp.swift
+//  ExampleCoordinatorUI
+//
+//  Created by Ricardo Bailoni on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleCoordinatorUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
